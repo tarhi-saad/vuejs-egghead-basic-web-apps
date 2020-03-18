bd853bd5afd6484befd64204f96aac84f54f3c2e
Saad Tarhi <saad.trh@gmail.com>
2020-03-19 00:33:13 +0100

    Create a List Component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,26 @@
 <template>
-  <v-card width="400" class="mx-auto mt-5">
-    <v-card-title>{{ title }}</v-card-title>
-    <v-card-text v-html="content"></v-card-text>
+  <v-card width="auto" class="mx-auto mt-5">
+    <!-- <v-card-title>{{ title }}</v-card-title> -->
+    <v-list>
+      <v-subheader>{{ title }}</v-subheader>
+      <v-form @submit.prevent="addItem">
+        <v-text-field v-model="textInput" class="mx-4" label="Add item" prepend-inner-icon="add"></v-text-field>
+      </v-form>
+      <v-list-item-group color="primary">
+        <v-slide-y-transition group tag="v-list">
+          <v-list-item v-for="(item, index) in items" :key="item.text">
+            <v-list-item-content>
+              <v-list-item-title>{{ item.text }}</v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon @click="removeItem(index)" color="deep-orange darken-1">delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-slide-y-transition>
+      </v-list-item-group>
+    </v-list>
   </v-card>
 </template>
 
@@ -9,9 +28,28 @@
 export default {
   name: "Card",
   data: () => ({
-    title: "My title",
-    content: "My <strong>awesome</strong> content is <em>on line</em>"
-  })
+    title: "Dinosaurs",
+    items: [
+      { text: "Frilled dragon" },
+      { text: "African wild cat" },
+      { text: "Malabar squirrel" },
+      { text: "Sugar glider" },
+      { text: "White-necked raven" }
+    ],
+    textInput: ""
+  }),
+  methods: {
+    addItem() {
+      if (!this.textInput) return;
+      this.items.push({
+        text: this.textInput
+      });
+      this.textInput = "";
+    },
+    removeItem(index) {
+      this.items.splice(index, 1);
+    }
+  }
 };
 </script>
 
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,14 @@
+import "@mdi/font/css/materialdesignicons.css";
 import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
-const opts = {};
+const opts = {
+  icons: {
+    iconfont: "mdi" // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  }
+};
 
 export default new Vuetify(opts);
